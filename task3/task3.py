7a072134e56afa32b1be0fd814710c8fefca01ae
dyavall <137233329+dyavall@users.noreply.github.com>
2025-03-23 20:20:47 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,33 +1,41 @@
 import json
-def add(id,result):
-        for i in result:
+import argparse
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('values')
+    parser.add_argument ('tests')
+    parser.add_argument ('report')
+    return parser
+
+def add(id,values):
+        for i in values:
             if id == i["id"]:
                  return i["value"]
             else: continue
 
-def cycle(list,result1):
+def cycle(list,values):
      for i in list:
           id = i["id"]
-          add1 = add(id,result1)
+          add1 = add(id,values)
           i["value"]=add1
           try:
-               cycle(i["values"],result1)
+               cycle(i["values"],values)
           except:
                continue
      return list
+def main():
+     parser = createParser().parse_args()
+     with open(parser.tests,'r') as OpFile1:
+        tests = json.load(OpFile1)
+     with open(parser.values,'r') as OpFile2:
+        values = json.load(OpFile2)
 
-def main(file1,file2,report):
-    with open(file1,'r') as OpFile1:
-        result1 = json.load(OpFile1)
-    with open(file2,'r') as OpFile2:
-        result2 = json.load(OpFile2)
-    ppp = list(result1["tests"])
-    res = list(result2["values"])
-    result = cycle(ppp,res)
-    result1["tests"]=result
-    record = json.dumps(result1)
-    with open(report, "w") as file:
-        file.write(record)
+     result = cycle(list(tests["tests"]),list(values["values"]))
+     tests["tests"]=result
+     repord = json.dumps(tests)
+     with open(parser.report, "w") as file:
+        file.write(repord)
 
 
-main('tests.json','values.json','result.json')
+main()
